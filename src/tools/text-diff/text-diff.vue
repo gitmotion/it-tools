c32e874f27390432f5d81a3d47ff4bd94d32a63b
chadmin <chrishancode@gmail.com>
2024-10-23 10:58:27 -0700

    Reverting text-diff-not-loading

--- src/tools/text-diff/text-diff.vue
+++ src/tools/text-diff/text-diff.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import CDiffEditor from '@/ui/c-diff-editor/c-diff-editor.vue';
-</script>
-
 <template>
-  <c-card w-full important:flex-1 important:pa-1>
-    <CDiffEditor />
+  <c-card w-full important:flex-1 important:pa-0>
+    <c-diff-editor />
   </c-card>
 </template>
